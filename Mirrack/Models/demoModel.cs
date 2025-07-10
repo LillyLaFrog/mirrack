2d8d8676b44be7cf7592f5717829f2cadf3b00f7
Lilly Williams <williams.bryson00@gmail.com>
2025-07-10 15:10:46 -0600

    create mock module model

---
+++ Mirrack/Models/demoModel.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirrack.Models
+{
+    internal class demoModel : Application
+    {
+        public bool button1State = false;
+        public bool button2State = false;
+        public bool button3State = false;
+        public int scrollLocation = 0;
+    }
+}
