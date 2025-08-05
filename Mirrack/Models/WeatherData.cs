75bddf781d897987902c12433f3b37c6066efebd
Lilly Williams <williams.bryson00@gmail.com>
2025-08-05 14:50:16 -0600

    add to weather template

--- Mirrack/Models/WeatherData.cs
+++ Mirrack/Models/WeatherData.cs
@@ -8,10 +8,24 @@ namespace Mirrack.Models
 {
     public class WeatherData
     {
+        public WeatherData()
+        {
+            //create default weatherData
+            units = "us";
+            icon = "cloudy";
+            summary = "Overcast";
+            temperature = 70.13;
+            temperatureHigh = 75.23;
+            temperatureLow = 65.72;
+            precipProbability = 0.25;
+            windSpeed = 7.2;
+        }
         public string? units { get; set; }
         public string? icon { get; set; }
         public string? summary { get; set; }
         public double temperature { get; set; }
+        public double temperatureHigh { get; set; }
+        public double temperatureLow { get; set; }
         public double precipProbability { get; set; }
         public double windSpeed { get; set; }
     }
--- Mirrack/Views/Controls/WeatherWidget.axaml.cs
+++ Mirrack/Views/Controls/WeatherWidget.axaml.cs
@@ -1,11 +1,36 @@
+using Avalonia;
 using Avalonia.Controls.Primitives;
+using Mirrack.Models;
+using System;
+using System.Reactive;
+using System.Xml;
 
 namespace Mirrack.Controls;
 
 public class WeatherWidget : TemplatedControl
 {
-    public WeatherWidget()
-    {
-        
-    }
+
+    //displayed datapoints
+    public static readonly StyledProperty<string> IconProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(Icon));
+    public static readonly StyledProperty<string> SummaryProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(Summary));
+    public static readonly StyledProperty<string> TemperatureProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(Temperature));
+    public static readonly StyledProperty<string> TemperatureHighProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(TemperatureHigh));
+    public static readonly StyledProperty<string> TemperatureLowProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(TemperatureLow));
+    public static readonly StyledProperty<string> PrecipProbabilityProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(PrecipProbability));
+    public static readonly StyledProperty<string> WindSpeedProperty =
+        AvaloniaProperty.Register<WeatherWidget, string>(nameof(WindSpeed));
+
+    public string Icon { get => GetValue(IconProperty); set => SetValue(IconProperty, value); }
+    public string Summary { get => GetValue(SummaryProperty); set => SetValue(SummaryProperty, value); }
+    public string Temperature { get => GetValue(TemperatureProperty); set => SetValue(TemperatureProperty, value); }
+    public string TemperatureHigh { get => GetValue(TemperatureHighProperty); set => SetValue(TemperatureHighProperty,value); }
+    public string TemperatureLow { get => GetValue(TemperatureLowProperty); set => SetValue(TemperatureLowProperty, value); }
+    public string PrecipProbability { get => GetValue(PrecipProbabilityProperty); set => SetValue(PrecipProbabilityProperty, value); }
+    public string WindSpeed { get => GetValue(WindSpeedProperty); set => SetValue(WindSpeedProperty, value); }
 }
